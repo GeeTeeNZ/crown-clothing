70f9f7eb2eb0eb95a7d8695d7e70dfe1da0b6fdb
GeeTeeNZ <gee-tee@windowslive.com>
2021-06-20 17:09:54 +1200

    added checkout page, checkout item component and updated cart to use selectors

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
 import './App.css'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import CheckoutPage from './pages/checkout/checkout.component'
 import Header from './components/header/header.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import { setCurrentUser } from './redux/user/user.actions'
+import { selectCurrentUser } from './redux/user/user.selectors'
 
 class App extends React.Component {
-  
+
   unsubscribeFromAuth = null
 
   componentDidMount() {
@@ -24,15 +27,15 @@ class App extends React.Component {
 
         userRef.onSnapshot(snapShot => {
           setCurrentUser({
-              id: snapShot.id,
-              ...snapShot.data()
-            })
+            id: snapShot.id,
+            ...snapShot.data()
           })
-      } 
+        })
+      }
 
-      setCurrentUser(userAuth)        
+      setCurrentUser(userAuth)
     })
-  } 
+  }
 
   componentWillUnmount() {
     this.unsubscribeFromAuth()
@@ -43,15 +46,16 @@ class App extends React.Component {
       <div>
         <Header />
         <Switch>
-          <Route exact path='/' component={HomePage}/>
-          <Route path='/shop' component={ShopPage}/>
-          <Route 
-            exact 
-            path='/signin' 
-            render = {() => 
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
+          <Route
+            exact
+            path='/signin'
+            render={() =>
               this.props.currentUser ? (
                 <Redirect to='/' />
-              ) :(
+              ) : (
                 <SignInAndSignUpPage />
               )
             }
@@ -60,18 +64,17 @@ class App extends React.Component {
       </div>
     )
   }
-} 
+}
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 export default connect(
-  mapStateToProps, 
+  mapStateToProps,
   mapDispatchToProps
-  )(App)
- 
+)(App)
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
 import { auth } from '../../firebase/firebase.utils'
 
-import { ReactComponent  as Logo } from '../../assets/crown.svg'
+import { ReactComponent as Logo } from '../../assets/crown.svg'
 import CartIcon from '../cart-icon/cart-icon.component'
 import CartDropdown from '../cart-dropdown/cart-dropdown.component'
+import { selectCartHidden } from '../../redux/cart/cart.selectors'
+import { selectCurrentUser } from '../../redux/user/user.selectors'
 
 import './header.styles.scss'
 
- const Header = ({ currentUser, hidden }) => (
-     <div className='header'>
+const Header = ({ currentUser, hidden }) => (
+    <div className='header'>
         <Link className='logo-container' to="/" >
             <Logo className='logo' />
-        </Link> 
+        </Link>
         <div className='options'>
             <Link className='option' to='/shop'>
                 SHOP
@@ -23,23 +26,23 @@ import './header.styles.scss'
                 CONTACT
             </Link>
             {currentUser ? (
-              <div className='option' onClick={() => auth.signOut()} >
-                  SIGN OUT 
-              </div>
-            )   : (
-              <Link className='option' to='/signin'>
-                  SIGN IN 
-              </Link>
+                <div className='option' onClick={() => auth.signOut()} >
+                    SIGN OUT
+                </div>
+            ) : (
+                <Link className='option' to='/signin'>
+                    SIGN IN
+                </Link>
             )}
             <CartIcon />
         </div>
-        {hidden ? null : <CartDropdown /> }
-     </div>
- )
+        {hidden ? null : <CartDropdown />}
+    </div>
+)
 
- const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
-     currentUser,
-     hidden
- })
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
+})
 
- export default connect(mapStateToProps)(Header)
+export default connect(mapStateToProps)(Header)
