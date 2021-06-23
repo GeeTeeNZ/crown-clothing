72344ad675a2d8b0236e97a965003d84322d245a
GeeTeeNZ <gee-tee@windowslive.com>
2021-06-24 10:16:44 +1200

    Implemented Stripe integration and updated favicon & title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import StripeCheckout from 'react-stripe-checkout'
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100
+    const publishableKey = 'pk_test_51J539xL2a2Jw6O3CVcQCdfJrtJwEuT231E3ERRaQycz5idFSz54TufpDijiS8pYl3qUpdtDkxBzWHlw34kO9rRY200uOW5YUEV'
+
+    const onToken = token => {
+        console.log(token)
+        alert('Payment Successful')
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='CROWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton
